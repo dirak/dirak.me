53c85a6c91cac0792a507d470ac9d487648cfe16
dirak <dirak.social@gmail.com>
2019-02-17 16:26:30 -0500

    test

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
-const https = require('https');
-const express = require('express');
-const fs = require('fs');
-const helmet = require('helmet');
+const https = require('https')
+const express = require('express')
+const fs = require('fs')
+const helmet = require('helmet')
 
-require('dotenv').config();
+require('dotenv').config()
 
 const options = {
 	key: fs.readFileSync(process.env.KEYPATH),
 	cert: fs.readFileSync(process.env.CERTPATH)
-};
+}
 
-const app = express();
+const app = express()
 
-app.use(helmet());
-app.use(express.static(process.env.MODULEPATH));
+app.use(helmet())
+app.use(express.static(process.env.MODULEPATH))
+app.get('/', (req, res) => {
+	console.log(req.hostname)
+})
+app.listen(process.env.APPPORT)
 
-app.listen(process.env.APPPORT);
-
-https.createServer(options, app).listen(process.env.PORT);
+https.createServer(options, app).listen(process.env.PORT)
